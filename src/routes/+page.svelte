846f59f112370022bae3d2c5f67278c990e6e0a6
Josh Zappone <zappone.josh@gmail.com>
2023-12-21 11:46:36 -0800

    wrap home content in column

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
-<h1>Welcome to Advent of Svelte</h1>
-<p>This project uses Svelte Kit and Svelte 4.</p>
-<p>
-	Visit <a target="_blank" href="https://advent.sveltesociety.dev">Advent of Svelte</a> or read the readme
-	to view the challenges.
-</p>
+<div class="column">
+	<h1>Welcome to Advent of Svelte</h1>
+	<p>This project uses Svelte Kit and Svelte 4.</p>
+	<p>
+		Visit <a target="_blank" href="https://advent.sveltesociety.dev">Advent of Svelte</a> or read the
+		readme to view the challenges.
+	</p>
 
-<ul>
-	<li><a href="/day1">See day 1</a></li>
-</ul>
+	<ul>
+		<li><a href="/day1">See day 1</a></li>
+	</ul>
+</div>
